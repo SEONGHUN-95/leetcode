c92a4e6ed187695800df6aecce1f5b0f49135864
Jack <60609613+SEONGHUN-95@users.noreply.github.com>
2023-07-07 13:35:43 +0900

    Time: 0 ms (100.00%), Space: 41 MB (70.80%) - LeetHub

---
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int findMin(int[] nums) {
+        return findMinRecursive(nums, 0, nums.length -1);
+    }
+    private int findMinRecursive(int[] nums, int left, int right){
+        int mid = (left+right)/2;
+        
+        if (left == right){
+            return nums[left];
+        }
+        
+        if (nums[mid]>nums[right]) {
+            return findMinRecursive(nums, mid+1, right);
+        }
+        
+        else if(nums[mid]<nums[right]) {
+            return findMinRecursive(nums, left, mid);
+        }
+        
+        return nums[left];
+    }
+}
